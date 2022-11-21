d43db96809f65a61ef7887f5ff8a5cdf5694acf9
Pedro Papini <ppapinia@gmail.com>
2022-11-21 19:23:21 -0300

    Resolve requisito 12 - Cria a classe Dragon

---
+++ src/Dragon.ts
@@ -0,0 +1,60 @@
+import { SimpleFighter } from './Fighter';
+import Monster from './Monster';
+
+type DragonAttack = {
+  name: string
+  modifier: number,
+  alert: string,
+  requiredRoll: number
+};
+
+export default class Dragon extends Monster {
+  private attacks: DragonAttack[] = [
+    {
+      name: 'stomp',
+      modifier: 1,
+      alert: 'The dragon stomps the player!',
+      requiredRoll: 0,
+    },
+    {
+      name: 'tail whip',
+      modifier: 1.2,
+      alert: 'The dragon whips its tail against the player!',
+      requiredRoll: 0.2,
+    },
+    {
+      name: 'claw',
+      modifier: 1.4,
+      alert: 'The dragon attacks the player with its sharp claws!',
+      requiredRoll: 0.4,
+    },
+    {
+      name: 'spit fire',
+      modifier: 2,
+      alert: 'With a thundering roar, the dragon spits fire at the player!',
+      requiredRoll: 0.9,
+    },
+  ];
+
+  private orderedAttacks = this.attacks
+    .sort((a, b) => b.requiredRoll - a.requiredRoll);
+
+  constructor(protected _lifePoints = 999, protected _strength = 100) {
+    super(_lifePoints, _strength);
+  }
+
+  private getDragonAttack(): DragonAttack {
+    const attackRoll = Math.random();
+    const dragonAttack = this.orderedAttacks
+      .find((attackOption) => attackOption.requiredRoll >= attackRoll)
+      || this.attacks[0];
+    return dragonAttack;
+  }
+
+  public attack(enemy: SimpleFighter): void {
+    const dragonAttack = this.getDragonAttack();
+    const attackValue = this._strength * dragonAttack.modifier;
+    console.log(dragonAttack.alert);
+    enemy.receiveDamage(attackValue);
+  }
+}
--- src/Monster.ts
+++ src/Monster.ts
@@ -1,7 +1,7 @@
 import { SimpleFighter } from './Fighter';
 
 export default class Monster implements SimpleFighter {
-  constructor(private _lifePoints = 85, private _strength = 63) {}
+  constructor(protected _lifePoints = 85, protected _strength = 63) {}
 
   get lifePoints() { return this._lifePoints; }
   get strength() { return this._strength; }
